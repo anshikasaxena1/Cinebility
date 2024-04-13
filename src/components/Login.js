c6c174e48981ae8a7158f20c3208c2563da3be08
Anshika Saxena <anshiksaxena310@gmail.com>
2024-04-13 15:39:37 +0530

    validating the email and password

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,12 +1,32 @@
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 import Header from './Header'
+import { checkValidData } from '../utils/validate'
 
 const Login = () => {
+  const [errorMessage,setErrorMessage]=useState();
+
   const [isSignInForm,setIsSignInForm]=useState(true);
 
+  const email=useRef(null);
+  const password=useRef(null);
+  
+
+  const handleButtonClick=()=>{
+    //validate the form data first
+    // checkValidData
+   
+    
+    const message=checkValidData(email.current.value,password.current.value);
+    
+    // const message=checkValidData(email.current.value,password.current.value,"mee chan");
+    // // {isSignInForm? message : message1};
+
+    setErrorMessage(message);
+    //sign in/sign up
+  }
   const toggleSignInForm=()=>{
     setIsSignInForm(!isSignInForm);
-  }
+  };
 
   return (
 
@@ -15,12 +35,13 @@ const Login = () => {
       <div>
       <img className='absolute' src="https://assets.nflxext.com/ffe/siteui/vlv3/c1366fb4-3292-4428-9639-b73f25539794/3417bf9a-0323-4480-84ee-e1cb2ff0966b/IN-en-20240408-popsignuptwoweeks-perspective_alpha_website_large.jpg" alt="logo"/>
       </div>
-      <form className=' p-12 bg-white w-4/12 absolute my-36 mx-auto right-0 left-0 rounded-lg bg-opacity-70'>
+      <form onSubmit={(e)=>e.preventDefault()} className=' p-12 bg-white w-4/12 absolute my-36 mx-auto right-0 left-0 rounded-lg bg-opacity-70'>
         <h1 className='font-bold text-3xl'>{isSignInForm ? "Sign In" : "Sign Up"}</h1>
         {!isSignInForm && <input type="text" placeholder="Full Name" className='p-4 my-4 w-full bg-stone-200'/>}
-        <input type="text" placeholder="Email Address" className='p-4 my-4 w-full bg-stone-200'/>
-        <input type="password" placeholder="Password" className='p-4 my-4 w-full bg-stone-200'/>
-        <button className='p-4 my-4 bg-orange-900 rounded-lg w-full'>{isSignInForm ? "Sign In" : "Sign Up"}</button>
+        <input ref={email} type="text" placeholder="Email Address" className='p-4 my-4 w-full bg-stone-200'/>
+        <input ref={password} type="password" placeholder="Password" className='p-4 my-4 w-full bg-stone-200'/>
+        <p className='text-red-800 '>{errorMessage}</p>
+        <button className='p-4 my-4 bg-orange-900 rounded-lg w-full' onClick={handleButtonClick}>{isSignInForm ? "Sign In" : "Sign Up"}</button>
         <p className='p-4 my-4 cursor-pointer' onClick={toggleSignInForm}>{isSignInForm ? "New to Cinebility? Sign Up now" : "Already Registered? Sign In now..."}</p>
       </form>
       
